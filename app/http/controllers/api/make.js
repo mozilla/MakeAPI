0077804cca2cec7b94d8f2b997d32879080f573e
Simon Wex <simon@simonwex.com>
2013-01-08 17:11:55 -0800

    Adding paranoid delete functionality.

--- app/http/controllers/api/make.js
+++ app/http/controllers/api/make.js
@@ -4,14 +4,33 @@
 
 var
 models = require('./../../../models');
+logger = require('./../../../../lib/logger');
 Make   = models.Make;
+
+function handleError(resp, err, code){
+  if (code >= 500){
+    logger.error(err);
+  }
+  resp.writeHead(code, {'Content-Type': 'application/json'});
+  resp.write(JSON.stringify(err));
+  return resp.end();
+}
+
+function handleSave(resp, err, make){
+  if (err){
+    return handleError(resp, err, 400);
+  }
+  else{
+    return resp.send(make);
+  }
+}
 
 exports.find = function(req, resp){
   //TODO: enable filtering based on query params.
 
-  return Make.find(function(err, makes){
+  return Make.find().where('deletedAt', null).exec(function(err, makes){
     if (err){
-      return console.log(err);
+      return handleError(resp, err, 500);
     }
     else{
       // resp.writeHead(200, {'Content-Type': 'application/json'});
@@ -21,26 +40,21 @@ exports.find = function(req, resp){
 };
 
 exports.findById = function(req, resp){
-  return Make.findById(req.params.id, function(err, make) {
-    if (!err){
-      return resp.send(make);
+  return Make.findById(req.params.id).where('deletedAt', null).exec(function(err, make) {
+    if (err){
+      if (err.name === 'CastError'){
+        err.message = "The supplied value does not look like a Make ID.";
+        return handleError(resp, err, 400);
+      }
+      else{
+        return handleError(resp, err, 500);
+      }
     }
     else{
-      return console.log(err);
+      return resp.send(make);
     }
   });
 };
-
-function handleSave(resp, err, make){
-  if (err){
-    resp.writeHead(400, {'Content-Type': 'application/json'});
-    resp.write(JSON.stringify(err));
-    return resp.end();
-  }
-  else{
-    return resp.send(make);
-  }
-}
 
 exports.create = function(req, resp){
   var make = new Make();
@@ -56,7 +70,7 @@ exports.create = function(req, resp){
 };
 
 exports.update = function(req, resp){
-  Make.findById(req.params.id, function(err, make){
+  Make.findById(req.params.id).where('deletedAt', null).exec(function(err, make){
     for (var i in Make.publicFields){
       var field = Make.publicFields[i];
       if (req.body.indexOf(field)){
@@ -70,5 +84,25 @@ exports.update = function(req, resp){
 };
 
 exports.delete = function(req, resp){
-  // TODO: set deletedAt and add to all queries.
+  return Make.findById(req.params.id).where('deletedAt', null).exec(function(err, make) {
+    if (err){
+      if (err.name === 'CastError'){
+        err.message = "The supplied value does not look like a Make ID.";
+        return handleError(resp, err, 400);
+      }
+      else{
+        return handleError(resp, err, 500);
+      }
+    }
+
+    if (make){
+      make.deletedAt = (new Date()).getTime();
+      make.save(function(err, make){
+        if (err){
+          return handleError(resp, err, 500);
+        }
+      });
+    }
+    return resp.send(make);
+  });
 };
