12d202abd5dcca04af3629150365a4eec62a5532
Matthew Schranz <schranz.m@gmail.com>
2013-04-12 14:34:54 -0400

    Rework of API

--- app/http/public/Make.js
+++ app/http/public/Make.js
@@ -51,11 +51,65 @@
         makeAPI = options.makeAPI;
       }
 
-      if ( options.makeAPI ) {
-        makeAPI = options.makeAPI;
-      }
-
       return this;
+    },
+
+    all: {
+      searchFilters: [],
+      size: DEFAULT_SIZE,
+      withAuthor: function( name ) {
+        this.searchFilters.push({
+          query: {
+            field: {
+              author: name
+            }
+          }
+        });
+        return this;
+      },
+      withTags: function( tags, execution ) {
+        this.searchFilters.push({
+          terms: {
+            tags: tags,
+            execution: execution
+          }
+        });
+        return this;
+      },
+      withFields: function( fields ) {
+        return this;
+      },
+      withLimit: function( num ) {
+        this.size = parseInt( num ) || DEFAULT_SIZE;
+        return this;
+      },
+      then: function( callback ) {
+        var searchQuery = {
+              query: {
+                filtered: {
+                  query: {
+                    match_all: {}
+                  },
+                  filter: {
+                    and: {
+                      filters: []
+                    }
+                  }
+                }
+              },
+              size: this.size
+            },
+            searchFilters = this.searchFilters;
+
+        for ( var i = 0; i < searchFilters.length; i++ ) {
+          searchQuery.query.filtered.filter.and.filters.push( searchFilters[ i ] );
+        }
+
+        this.size = DEFAULT_SIZE;
+        this.searchFilters = [];
+
+        doXHR( "POST", "/api/makes/search", searchQuery, callback );
+      }
     },
 
     createMake: function createMake( makeOptions, callback ) {
@@ -70,125 +124,6 @@
 
     deleteMake: function deleteMake( id, callback ) {
       doXHR( "DELETE", "/api/make/" + id, callback );
-      return this;
-    },
-
-    /**
-     * [getLatestMakes - Get the most recently created Makes]
-     * @param  {[Object]} options
-     * @return {[Make]}
-     */
-    getLatestMakes: function getLatestMakes( options ) {
-      if ( !querySet ) {
-        options = options || {};
-        searchOptions.push( { query: options, type: "match_all" } );
-        querySet = true;
-      }
-      return this;
-    },
-
-    /**
-     * [getAuthorsMakes - Get Makes by Author Name]
-     * @param  {[Object]} options
-     * @return {[Make]}
-     */
-    getAuthorsMakes: function getAuthorsMakes( options ) {
-      if ( !querySet ) {
-        options = options || {};
-        searchOptions.push( { query: options, type: "field" } );
-        querySet = true;
-      }
-      return this;
-    },
-
-    /**
-     * [getByTags - Get Makes by tags]
-     * @param  {[Object]} options
-     * @return {[Make]}
-     */
-    getByTags: function getByTags( options ) {
-      if ( !querySet ) {
-        options = options || {};
-        searchOptions.push( { query: options, type: "terms" } );
-        querySet = true;
-      }
-      return this;
-    },
-
-    /**
-     * [withTags - Add a filter for specific tags]
-     * @param  {[type]} options
-     * @return {[Make]}
-     */
-    withTags: function withTags( options ) {
-      if ( querySet ) {
-        options = options || {};
-        searchOptions.push( { filter: options, type: "terms" } );
-      }
-      return this;
-    },
-
-    /**
-     * [size - Specifies the amount of results to be returned by the API]
-     * @param  {[Integer]} num
-     * @return {[Make]}
-     */
-    size: function size( num ){
-      if ( querySet ) {
-        num = parseInt( num ) || DEFAULT_SIZE;
-        searchOptions.push( { size: num } );
-      }
-      return this;
-    },
-
-    /**
-     * [run - Make the call to the MakeAPI]
-     * @param  {[Function]} callback
-     * @return {[type]}
-     */
-    run: function run( callback ) {
-      var searchQuery = { query: {} },
-          initialQuery, options, type, filterObj;
-
-      callback = callback || function(){};
-
-      if ( querySet ) {
-        querySet = false;
-        for ( var i = 0; i < searchOptions.length; i++ ) {
-          options = searchOptions[ i ];
-
-          if ( options.hasOwnProperty( "query" ) ) {
-            searchQuery.query[ options.type ] = options.query;
-            initialQuery = options;
-          } else if ( options.hasOwnProperty( "filter" ) ) {
-            type = options.type;
-
-            if ( !searchQuery.filtered ) {
-              searchQuery.query = {};
-              searchQuery.query.filtered = {};
-              searchQuery.query.filtered.query = {};
-              searchQuery.query.filtered.query[ initialQuery.type ] = initialQuery.query;
-              searchQuery.query.filtered.filter = { and: [] };
-              filterObj = {};
-              filterObj[ type ] = options.filter;
-              searchQuery.query.filtered.filter.and.push( filterObj );
-            } else {
-              filterObj = {};
-              filterObj[ type ] = options.filter;
-              searchQuery.query.filtered.filter.and.push( filterObj );
-            }
-          } else if ( options.hasOwnProperty( "size" ) ) {
-            searchQuery.size = options.size;
-          }
-        }
-
-        searchOptions = [];
-
-        doXHR( "POST", "/api/makes/search", searchQuery, callback );
-      } else {
-        callback( { error: "No base query was set" } );
-      }
-
       return this;
     }
 
