7fc256586350369dbcfb54d4f9a04af7306d4505
Christopher De Cairos <christopherd@mozillafoundation.org>
2013-08-02 10:47:19 -0400

    Bug 885384 - Write custom paging tool for make editor, use local versions of script dependencies, add font-awesome for MakePager UI

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,26 +4,26 @@
 
 $(function() {
 
-  var Slick = window.Slick,
+  var Slick = window.Slick;
 
-      formatters = {
-        date: function( row, cell, val ) {
-          var newDate;
+  var FORMATTERS = {
+    date: function( row, cell, val ) {
+      var newDate;
 
-          try {
-            newDate = val ? new Date( val ) : "N/A";
-          } catch( e ) {
-            newDate = Date.now();
-            errorSpan.removeClass( "hidden" ).html( "Bad Date Value. Falling back to current date and time." );
-          }
-          return newDate;
-        },
-        tags: function( row, cell, val ) {
-          return Array.isArray( val ) ? val.join( "," ) : val;
-        }
-      };
+      try {
+        newDate = val ? new Date( val ) : "N/A";
+      } catch( e ) {
+        newDate = Date.now();
+        errorSpan.removeClass( "hidden" ).html( "Bad Date Value. Falling back to current date and time." );
+      }
+      return newDate;
+    },
+    tags: function( row, cell, val ) {
+      return Array.isArray( val ) ? val.join( "," ) : val;
+    }
+  },
 
-  var COLUMNS = [
+  COLUMNS = [
     {
       id: "id",
       name: "Click To Delete",
@@ -70,34 +70,165 @@ $(function() {
     { id: "username", name: "username", field: "username",
       sortable: true },
     { id: "tags", name: "Tags", field: "tags",
-      formatter: formatters.tags,
+      formatter: FORMATTERS.tags,
       editor: Slick.Editors.Text
     },
     { id: "remixedFrom", name: "Remixed From", field: "remixedFrom",
       sortable: true },
     { id: "createdAt", name: "Created At", field: "createdAt",
-      formatter: formatters.date,
+      formatter: FORMATTERS.date,
       editor: Slick.Editors.Date,
       sortable: true
     },
     { id: "updatedAt", name: "Updated At", field: "updatedAt",
-      formatter: formatters.date,
+      formatter: FORMATTERS.date,
       editor: Slick.Editors.Date,
       sortable: true
     }
   ];
 
-  // max hits that ES should return on a search.
-  var MAX_SIZE = 1000;
+  var MakePager = function( settings ) {
 
-  var options = {
-        autoEdit: false,
-        editable: true,
-        enableTextSelectionOnCells: true,
-        defaultColumnWidth: 150,
-        topPanelHeight: 200
-      },
-      csrfToken = $("meta[name=csrf_token]").attr("content"),
+    var STATUS_TEMPLATE = "Page {{pagenum}} of {{pagetotal}} - {{hits}} total hits",
+        PAGE_SIZE = 100;
+
+    var goFirst = settings.goFirst,
+        goPrevious = settings.goPrevious,
+        goNext = settings.goNext,
+        goLast = settings.goLast,
+        goToInput = settings.goToInput,
+        goToBtn = settings.goToBtn,
+        navStatus = settings.navStatus,
+        errorElem = settings.errorElement,
+        currentQuery = {},
+        currentPage = 1,
+        totalPages = 1;
+
+    function trimItems( items ) {
+      return items.map( function( item ) {
+        return item.trim();
+      });
+    }
+
+    function setQuery( type, query ) {
+      currentQuery.type = type;
+      currentQuery.query = query;
+    }
+
+    function handleMakes( err, data, total, page ) {
+      if ( err || !data ) {
+        errorElem.classList.remove( "hidden" );
+        errorElem.textContent = "Error retrieving data: " + err;
+      } else if ( !data.length  ) {
+        data = [];
+        totalPages = 1;
+        currentPage = 1;
+        total = 0;
+      } else {
+        totalPages = Math.ceil( total / PAGE_SIZE );
+        currentPage = page;
+      }
+
+      navStatus.textContent = STATUS_TEMPLATE
+      .replace( "{{pagenum}}" , currentPage )
+      .replace( "{{pagetotal}}", totalPages )
+      .replace( "{{hits}}", total );
+
+      dataView.beginUpdate();
+      dataView.setItems( data );
+      dataView.endUpdate();
+      grid.render();
+    }
+
+    function goToPage( num ) {
+
+      if ( currentQuery.type && currentQuery.query ) {
+        if ( currentQuery.type === "tags" ) {
+          currentQuery.query = trimItems( currentQuery.query.split( "," ) );
+        }
+        make[ currentQuery.type ]( currentQuery.query );
+      }
+
+      make.limit( PAGE_SIZE )
+      .page( num )
+      .sortByField( "createdAt", "desc" )
+      .then(function( err, data, total ) {
+        handleMakes( err, data, total, num );
+      });
+    }
+
+    function setPage( num ) {
+      num = num ? +num : 1;
+      if ( num < 1 || num > totalPages ) {
+        return;
+      }
+      goToPage( num );
+    }
+
+    function checkInputRange( val ) {
+      if ( val <= 0 ) {
+        goToInput.value = 1;
+        return 1;
+      } else if ( val > totalPages ) {
+        goToInput.value = totalPages;
+        return totalPages;
+      }
+      return val;
+    }
+
+    function validInput( val ) {
+      if ( !val || isNaN( val ) ) {
+        goToInput.classList.add( "invalid-input" );
+        return false;
+      }
+      goToInput.classList.remove( "invalid-input" );
+      return true;
+    }
+
+    goFirst.addEventListener( "click", function() {
+      setPage( 1 );
+    }, false );
+
+    goPrevious.addEventListener( "click", function() {
+      setPage( currentPage - 1 );
+    }, false );
+
+    goNext.addEventListener( "click", function() {
+      setPage( currentPage + 1 );
+    }, false );
+
+    goLast.addEventListener( "click", function() {
+      setPage( totalPages );
+    }, false );
+
+    goToInput.addEventListener( "keypress", function( e ) {
+      var val;
+      if ( e.which === 13 ) {
+        e.preventDefault();
+        e.stopPropagation();
+        val = +goToInput.value;
+        if ( !validInput( val ) ) {
+          return;
+        }
+        setPage( checkInputRange( val ) );
+      }
+    }, false );
+
+    goToBtn.addEventListener( "click", function() {
+      var val = +goToInput.value;
+      if ( !validInput( val ) ) {
+        return;
+      }
+      setPage( checkInputRange( val ) );
+    }, false );
+
+    this.goToPage = goToPage;
+    this.setQuery = setQuery;
+
+    return this;
+  };
+
+  var csrfToken = $("meta[name=csrf_token]").attr("content"),
       make = window.Make({
         apiURL: "/admin",
         csrf: csrfToken
@@ -109,11 +240,23 @@ $(function() {
       identity = $( "#identity" ).text(),
       errorSpan = $( ".error-message" ),
       dataView = new Slick.Data.DataView(),
-      grid = new Slick.Grid( gridArea, dataView, COLUMNS, options ),
-      pager = new Slick.Controls.Pager( dataView, grid, $( "#pager" ) ),
-      data;
-
-  dataView.setPagingOptions( { pageSize: 25 } );
+      grid = new Slick.Grid( gridArea, dataView, COLUMNS, {
+        autoEdit: false,
+        editable: true,
+        enableTextSelectionOnCells: true,
+        defaultColumnWidth: 150,
+        topPanelHeight: 200
+      }),
+      pager = new MakePager({
+        goFirst: document.querySelector( "#nav-first" ),
+        goPrevious: document.querySelector( "#nav-previous"),
+        goNext: document.querySelector( "#nav-next" ),
+        goLast: document.querySelector( "#nav-last" ),
+        goToInput: document.querySelector( "#nav-go-to-page"),
+        goToBtn: document.querySelector( "#nav-go-to-page-btn"),
+        navStatus: document.querySelector( "#nav-status" ),
+        errorElement: errorSpan
+      });
 
   window.removeClick = function( id, dataViewId ){
     make.remove( id, function( err ) {
@@ -128,17 +271,9 @@ $(function() {
     });
   };
 
-  function trimItems( items ) {
-    return items.map( function( item ) {
-      return $.trim( item );
-    });
-  }
-
   grid.onCellChange.subscribe(function ( e, data ) {
     var make = data.item;
-
     make.tags = Array.isArray( make.tags ) ? make.tags : trimItems( make.tags.split( "," ) );
-
     make.update( identity, function( err, updated ) {
       if ( err ) {
         errorSpan.removeClass( "hidden" ).html( "Error Updating! " + JSON.stringify( err ) );
@@ -162,33 +297,10 @@ $(function() {
     dataView.fastSort( data.sortCol.field, data.sortAsc );
   });
 
-  function createGrid( err, data ) {
-    if ( err ) {
-      errorSpan.removeClass( "hidden" ).html( "Error retrieving data: " + err );
-      return;
-    }
-    dataView.beginUpdate();
-    dataView.setItems( data );
-    dataView.endUpdate();
-    grid.render();
-  }
-
   function doSearch() {
-    var searchType = searchTypeSelector.val(),
-        search = searchValue.val();
-
+    pager.setQuery( searchTypeSelector.val(), searchValue.val() );
     errorSpan.addClass( "hidden" ).html( "" );
-
-    if ( search ) {
-      if ( searchType === "tags" ) {
-        search = trimItems( search.split( "," ) );
-      }
-      make[ searchType ]( search );
-    }
-
-    make.limit( MAX_SIZE )
-    .sortByField( "createdAt", "desc" )
-    .then( createGrid );
+    pager.goToPage( 1 );
   }
 
   searchBtn.click( doSearch );
@@ -204,10 +316,8 @@ $(function() {
   });
 
   // On initial load, Query for all makes.
-  make
-  .limit( MAX_SIZE )
-  .sortByField( "createdAt", "desc" )
-  .then( createGrid );
+  pager.setQuery();
+  pager.goToPage( 1 );
 
   // SSO
   var logout = $( "#logout" );
