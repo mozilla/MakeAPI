24f9d74b128bcc7ef298b644f10fb406c7fa608a
Christopher De Cairos <cade@mozillafoundation.org>
2014-06-20 14:31:49 -0400

    Bug 1016489 - Update scripts/generateScripts.js

--- scripts/generateKeys.js
+++ scripts/generateKeys.js
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Invoke by calling:
-// node generateKeys <email@address.com:STRING> <NumberOfKeys:INT> <isAdmin:BOOL>
+// node generateKeys <email@address.com:STRING> <NumberOfKeys:INT> <isAdmin:BOOL> <domain:STRING>
 
 var habitat = require( "habitat" ),
     uuid = require( "uuid" ),
@@ -15,47 +15,42 @@ habitat.load();
 var env = new habitat(),
     contactEmail = process.argv[ 2 ],
     numPairs = +process.argv[ 3 ],
-    isAdmin = process.argv[ 4 ] === "true";
+    isAdmin = process.argv[ 4 ] === "true",
+    domain = process.argv[ 5 ],
+    apps = [];
 
 if ( !contactEmail || typeof contactEmail !== "string" || isNaN( numPairs ) || !numPairs ) {
   console.log( "Invalid CONTACT_EMAIL or NUM_KEY_PAIRS" );
   process.exit( 1 );
 }
 
-dbh = require( "../lib/mongoose" )( env, function( err ) {
-  if ( err ) {
-    console.log( err );
-    process.exit( 1 );
-  }
+dbh = require( "../lib/mongoose" )();
+ApiApp = require( "../lib/models/apiApp" )( dbh.mongoInstance() );
 
-  var ApiApp = require( "../lib/models/apiApp" )( env, dbh.mongoInstance() ),
-      apps = [];
+for ( var i = 0; i < numPairs; i++ ) {
+  apps.push( new ApiApp({
+    contact: contactEmail,
+    privatekey: uuid.v4(),
+    publickey: uuid.v4(),
+    domain: domain,
+    revoked: false,
+    admin: !!isAdmin // can edit all makes
+  }));
+}
 
-  for ( var i = 0; i < numPairs; i++ ) {
-    apps.push( new ApiApp({
-      contact: contactEmail,
-      privatekey: uuid.v4(),
-      publickey: uuid.v4(),
-      revoked: false,
-      admin: !!isAdmin // This means "Internal Webmaker"
-    }));
-
-  }
-
-  async.eachSeries( apps, function( app, cb ) {
-    app.save(function( err, app ){
-      if ( err ) {
-        return cb( err );
-      }
-      console.log( "Keys generated for %s\nPRIVATEKEY: %s\nPUBLICKEY: %s\nadmin: %s",
-                   contactEmail, app.privatekey, app.publickey, isAdmin );
-      cb();
-    });
-  }, function done( err ) {
+async.eachSeries( apps, function( app, cb ) {
+  app.save(function( err, app ){
     if ( err ) {
-      console.log( "Something went horribly wrong saving: ", err );
-      return process.exit( 1 );
+      return cb( err );
     }
-    process.exit();
+    console.log( "Keys generated for %s\nPRIVATEKEY: %s\nPUBLICKEY: %s\nadmin: %s",
+                 contactEmail, app.privatekey, app.publickey, isAdmin );
+    cb();
   });
+}, function done( err ) {
+  if ( err ) {
+    console.log( "Something went horribly wrong saving: ", err );
+    return process.exit( 1 );
+  }
+  process.exit();
 });
