a3f7ee89c0b7ab31530f526054a7328ace3d1a32
Christopher De Cairos <chris@chrisdecairos.ca>
2015-03-04 15:53:17 -0500

    Reveal all of the MakeAPIs faults

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,20 @@
 module.exports = function( grunt ) {
+
+  var jsbeautifyrc = grunt.file.readJSON("node_modules/mofo-style/linters/.jsbeautifyrc");
+  var jscsrc = grunt.file.readJSON("node_modules/mofo-style/linters/.jscsrc");
+  var jshintrc = grunt.file.readJSON("node_modules/mofo-style/linters/.jshintrc");
+
+  var javaScriptFiles = [
+    "Gruntfile.js",
+    "server.js",
+    "lib/**/*.js",
+    "public/js/*.js",
+    "routes/**/*.js",
+    "test/**/*.js"
+  ];
+  var jsbeautifyrcReadOnly = jsbeautifyrc;
+  jsbeautifyrcReadOnly.mode = "VERIFY_ONLY";
+
   grunt.initConfig({
     pkg: grunt.file.readJSON( "package.json" ),
 
@@ -9,23 +25,33 @@ module.exports = function( grunt ) {
       ]
     },
     jshint: {
-      options: {
-        es5: true,
-        newcap: false
+      options: jshintrc,
+      files: javaScriptFiles
+    },
+    jsbeautifier: {
+      modify: {
+        src: javaScriptFiles,
+        options: jsbeautifyrc
       },
-      files: [
-        "Gruntfile.js",
-        "server.js",
-        "lib/**/*.js",
-        "public/js/*.js",
-        "routes/**/*.js",
-        "test/**/*.js"
-      ]
+      verify: {
+        src: javaScriptFiles,
+        options: jsbeautifyrcReadOnly
+      }
+    },
+    jscs: {
+      src: javaScriptFiles,
+      options:jscsrc
     }
   });
 
   grunt.loadNpmTasks( "grunt-contrib-csslint" );
   grunt.loadNpmTasks( "grunt-contrib-jshint" );
+  grunt.loadNpmTasks( "grunt-jsbeautifier" );
+  grunt.loadNpmTasks( "grunt-jscs" );
 
-  grunt.registerTask( "default", [ "csslint", "jshint" ]);
+  grunt.registerTask( "clean", [ "jsbeautifier:modify" ] );
+
+  grunt.registerTask( "validate", [ "jsbeautifier:verify", "jshint", "jscs", "csslint" ] );
+
+  grunt.registerTask( "default", [ "validate" ] );
 };
